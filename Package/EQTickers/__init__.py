de022cb7ba1caab8c2ea9aa2c33a7ebdef290b54
ndjoli-nathan <116048276+ndjoli-nathan@users.noreply.github.com>
2025-01-18 03:31:50 +0100

    Add files via upload

---
+++ Package/EQTickers/__init__.py
@@ -0,0 +1,11 @@
+# This file is used to import all the functions from the main script to the package
+from .Scripts.Main import (
+    AvailableRegions,
+    FetchAllTickers,
+    FetchedInformations,
+    FetchedInformationsList,
+    FetchRegionsTickers,
+    SetKeys,
+    GetKeys,
+    ResetKeys,
+)
